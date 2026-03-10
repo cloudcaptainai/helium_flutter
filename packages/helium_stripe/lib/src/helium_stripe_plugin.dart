efbeaa0a2879777b2691955335bdc87c4751c353
Kyle Gorlick <kyle@kylegorlick.com>
2026-03-09 18:13:40 -0700

    add additional optional initialize params to stripe

--- packages/helium_stripe/lib/src/helium_stripe_plugin.dart
+++ packages/helium_stripe/lib/src/helium_stripe_plugin.dart
@@ -1,8 +1,8 @@
 import 'dart:developer';
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:helium_flutter/helium_flutter.dart';
 
 /// Helium Stripe One Tap Purchase integration for Flutter (iOS only).
@@ -35,10 +35,13 @@ class HeliumStripe {
     String currencyCode = 'USD',
     HeliumCallbacks? callbacks,
     HeliumPurchaseDelegate? purchaseDelegate,
+    Widget? fallbackPaywall,
+    String? customAPIEndpoint,
     String? customUserId,
     Map<String, dynamic>? customUserTraits,
     String? revenueCatAppUserId,
     String? fallbackBundleAssetPath,
+    HeliumEnvironment? environment,
     HeliumPaywallLoadingConfig? paywallLoadingConfig,
   }) async {
     if (!_isIOS) {
@@ -47,10 +50,13 @@ class HeliumStripe {
         apiKey: apiKey,
         callbacks: callbacks,
         purchaseDelegate: purchaseDelegate,
+        fallbackPaywall: fallbackPaywall,
+        customAPIEndpoint: customAPIEndpoint,
         customUserId: customUserId,
         customUserTraits: customUserTraits,
         revenueCatAppUserId: revenueCatAppUserId,
         fallbackBundleAssetPath: fallbackBundleAssetPath,
+        environment: environment,
         paywallLoadingConfig: paywallLoadingConfig,
       );
       return;
@@ -70,10 +76,13 @@ class HeliumStripe {
       apiKey: apiKey,
       callbacks: callbacks,
       purchaseDelegate: purchaseDelegate,
+      fallbackPaywall: fallbackPaywall,
+      customAPIEndpoint: customAPIEndpoint,
       customUserId: customUserId,
       customUserTraits: customUserTraits,
       revenueCatAppUserId: revenueCatAppUserId,
       fallbackBundleAssetPath: fallbackBundleAssetPath,
+      environment: environment,
       paywallLoadingConfig: paywallLoadingConfig,
     );
 
