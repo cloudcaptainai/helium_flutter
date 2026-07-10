64e1ee4d641e750e2d986356af41ac1c56b2e9b7
salami <340367+salami@users.noreply.github.com>
2026-07-10 07:17:33 +0000

    Update helium-swift SDK dependency to 4.5.4

--- packages/helium_flutter/lib/core/const/contants.dart
+++ packages/helium_flutter/lib/core/const/contants.dart
@@ -1,5 +1,5 @@
 // SDK version - keep in sync with pubspec.yaml
-const String heliumFlutterSdkVersion = '3.3.4';
+const String heliumFlutterSdkVersion = '3.3.5';
 
 //Native view type
 const String upsellViewForTrigger = 'upsellViewForTrigger';
